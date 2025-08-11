2734054f46422d2c14a88096ab02c0a00a101a61
Suketu Patel <suketu@usa.net>
2025-08-11 15:11:26 +1000

    Exercise 16_findTheOldest completed

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  let oldestPerson;
+  let oldestAge = 0;
+  for (let i = 0; i < people.length; i++) {
+    let personAge = 0;
+    const currentYear = new Date().getFullYear();
 
+    if ("yearOfDeath" in people[i]) {
+      // if person is dead
+      personAge = people[i].yearOfDeath - people[i].yearOfBirth;
+    } else {
+      // if person is still alive
+      personAge = currentYear - people[i].yearOfBirth;
+    }
+
+    if (oldestAge < personAge) {
+      oldestAge = personAge;
+      oldestPerson = people[i];
+    }
+  }
+  return oldestPerson;
 };
 
 // Do not edit below this line
