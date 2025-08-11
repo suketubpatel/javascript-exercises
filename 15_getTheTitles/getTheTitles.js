23e6a005be89e0ded667b087489236c8d2bbafd9
Suketu Patel <suketu@usa.net>
2025-08-11 14:41:46 +1000

    Exercise 15_getTheTitles completed

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function (books) {
+  //   let bookTitles = [];
+  //   books.forEach((book) => {
+  //     bookTitles.push(book.title);
+  //   });
+  //   return bookTitles;
 
+  return books.map((book) => book.title);
 };
 
 // Do not edit below this line
