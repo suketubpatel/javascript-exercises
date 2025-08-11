97d5fc17553c75116f186f70cbb22c3c8ea99c0a
Suketu Patel <suketu@usa.net>
2025-08-11 11:44:27 +1000

    Exercise 12_calculato has completed

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  return arr.reduce((previousNum, currentNum) => previousNum + currentNum, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((previousNum, currentNum) => previousNum * currentNum, 1);
 };
 
-const power = function() {
-	
+const power = function (num1, num2) {
+  return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  result = 1;
+  if (num === 0 || num === 1) {
+    return result;
+  } else {
+    for (let i = num; i > 1; i--) {
+      result *= i;
+    }
+    console.log("Resutl: " + result);
+    return result;
+  }
 };
 
 // Do not edit below this line
@@ -29,5 +38,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
