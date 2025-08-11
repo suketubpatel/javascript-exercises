adf99b803f91c4e3702f000178b864438ecbd028
Suketu Patel <suketu@usa.net>
2025-08-11 12:33:42 +1000

    Ecercise 13_palindromes completed

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  // Read only a to z, A = Z and 0 to 0 and return new string in lower case
+  let onlyCharStr = str
+    .match(/[a-zA-Z0-9]/g)
+    .join("")
+    .toLowerCase();
 
+  // reverse the string
+  let reverseOnlyCharStr = onlyCharStr.split("").reverse().join("");
+
+  // Compare both string and return boolean result
+  return onlyCharStr === reverseOnlyCharStr;
 };
 
 // Do not edit below this line
